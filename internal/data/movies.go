c740c6becfb17df1b48872bd3d6721c3df87a698
Usman Zaheer <usmanzaheer1995@gmail.com>
2023-07-06 17:41:29 +0200

    add update movies handle

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -70,7 +70,22 @@ func (m MovieModel) Get(id int64) (*Movie, error) {
 }
 
 func (m MovieModel) Update(movie *Movie) error {
-	return nil
+	query := `
+		update movies
+		set title = $1, year = $2, runtime = $3, genres = $4, version = version + 1
+		where id = $5
+		returning version
+	`
+
+	args := []any{
+		movie.Title,
+		movie.Year,
+		movie.Runtime,
+		pq.Array(movie.Genres),
+		movie.ID,
+	}
+
+	return m.DB.QueryRow(query, args...).Scan(&movie.Version)
 }
 
 func (m MovieModel) Delete(id int64) error {
