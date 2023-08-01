bc5849464661d6c750dbdedee5b070df6830c4d1
Usman Zaheer <usmanzaheer1995@gmail.com>
2023-08-01 17:23:58 +0200

    add cors functionality

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/usmanzaheer1995/greenlight/internal/jsonlog"
 	"github.com/usmanzaheer1995/greenlight/internal/mailer"
 	"os"
+	"strings"
 	"sync"
 	"time"
 
@@ -36,6 +37,9 @@ type config struct {
 		username string
 		password string
 		sender   string
+	}
+	cors struct {
+		trustedOrigins []string
 	}
 }
 
@@ -68,6 +72,11 @@ func main() {
 	flag.StringVar(&cfg.smtp.username, "smtp-username", "7e9aef236c80ba", "SMTP username")
 	flag.StringVar(&cfg.smtp.password, "smtp-password", "ec902b19c72f47", "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Greenlight <no-reply@greenlight.alexedwards.net>", "SMTP sender")
+
+	flag.Func("cors-trusted-origins", "Trusted CORS origins (space separated)", func(val string) error {
+		cfg.cors.trustedOrigins = strings.Fields(val)
+		return nil
+	})
 
 	flag.Parse()
 
