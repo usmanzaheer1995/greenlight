40fc5a7cb06c498519b0e882a2fd500b42caea63
Usman Zaheer <usmanzaheer1995@gmail.com>
2023-08-02 11:27:19 +0200

    add metrics using the expvar std package

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"context"
 	"database/sql"
+	"expvar"
 	"flag"
 	"github.com/usmanzaheer1995/greenlight/internal/data"
 	"github.com/usmanzaheer1995/greenlight/internal/jsonlog"
 	"github.com/usmanzaheer1995/greenlight/internal/mailer"
 	"os"
+	"runtime"
 	"strings"
 	"sync"
 	"time"
@@ -90,6 +92,17 @@ func main() {
 	defer db.Close()
 
 	logger.PrintInfo("database connection pool established", nil)
+
+	expvar.NewString("version").Set(version)
+	expvar.Publish("goroutines", expvar.Func(func() any {
+		return runtime.NumGoroutine()
+	}))
+	expvar.Publish("database", expvar.Func(func() any {
+		return db.Stats()
+	}))
+	expvar.Publish("timestamp", expvar.Func(func() any {
+		return time.Now().Unix()
+	}))
 
 	app := &application{
 		config: cfg,
